8a2be5faeaf25479a2b9cf8244bf970b2e46e555
Dmitry Sky <dmitry.sky@icloud.com>
2016-10-27 20:41:37 -0600

    add firebase to the mix

--- index.ios.js
+++ index.ios.js
@@ -4,6 +4,18 @@
  * @flow
  */
 
+import * as firebase from 'firebase';
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "<your-api-key>",
+  authDomain: "<your-auth-domain>",
+  databaseURL: "<your-database-url>",
+  storageBucket: "<your-storage-bucket>",,
+};
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+
 import React, { Component } from 'react';
 import {
   AppRegistry,
